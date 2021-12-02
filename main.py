6614a486d308fe3f8564a569601a680de68eb039
joelrc26 <jrodriguez2@institutmvm.cat>
2021-12-02 15:31:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+def main():
+    """
+## EJERCICIO MESES CON IF
+    mes = str(input("Introduce un mes del año: "))
+    if mes == "Febrero":
+        print("El mes introducido tiene 28 días")
+    elif mes == "Abril" or mes == "Junio" or mes == "Septiembre" or mes == "Noviembre":
+        print("El mes introducido tiene 30 días")
+    else:
+        print("El mes introducido tiene 31 días")
+    """
+"""
+    ## EJERCICIO MESES CON MATCHCASE
+mes = int(input("Introduce un mes del año: "))
+while mes < 1 or mes > 12:
+    mes = int(input("Introduce un mes del año: "))
+match mes:
+    case 1:
+        print("Has introducido el mes de Enero.\nTiene 31 dias")
+    case 2:
+        print("Has introducido el mes de Febrero.\nTiene 28 dias")
+    case 3:
+        print("Has introducido el mes de Marzo.\nTiene 31 dias")
+    case 4:
+        print("Has introducido el mes de Abril.\nTiene 30 dias")
+    case 5:
+        print("Has introducido el mes de Mayo.\nTiene 31 dias")
+    case 6:
+        print("Has introducido el mes de Junio.\nTiene 30 dias")
+    case 7:
+        print("Has introducido el mes de Julio.\nTiene 31 dias")
+    case 8:
+        print("Has introducido el mes de Agosto.\nTiene 31 dias")
+    case 9:
+        print("Has introducido el mes de Septiembre.\nTiene 30 dias")
+    case 10:
+        print("Has introducido el mes de Octubre.\nTiene 31 dias")
+    case 11:
+        print("Has introducido el mes de Noviembre.\nTiene 30 dias")
+    case 12:
+        print("Has introducido el mes de Diciembre.\nTiene 31 dias")
+"""
+"""
+    ## EJERCICIO QUE INTERCAMBIA DOS NUMEROS ENTEROS
+num1 = int(input("Introduce un numero que despues su valor sera intercambiado\n"))
+while num1 < 0:
+    num1 = int(input("Introduce un numero que despues su valor sera intercambiado\n"))
+    num2 = int(input("Introduce otro numero que despues su valor sera intercambiado con el anterior\n"))
+while num2 < 0:
+    num2 = int(input("Introduce otro numero que despues su valor sera intercambiado con el anterior\n"))
+aux = num1
+num1 = num2
+num2 = aux
+print("Valores intercambiados:", num1, num2)
+"""
+"""
+    ## EJERCICIO VALIDAR SI ES NUMERO NATURAL
+num1 = int(input("Introduce un número natural: "))
+while num1 < 1:
+    num1 = int(input("Introduce un número natural: "))
+print("El número introducido", [num1] ," es un número natural")
+"""
+"""
+    ## EJERCICIOS CONVERSIÓN DE GRADOS
+FARENHEIT = 32.00
+KELVIN = 273.15
+print("MENÚ")
+print("1. Convertir de grados Celsius a grados Farenheit")
+print("2. Convertir de grados Celsius a grados Kelvin")
+print("3. Salir")
+menu = int(input("Introduce un número del menú: "))
+while menu >= 4:
+    menu = int(input("Introduce un número del menú: "))
+    grados = int(input("Introduce la temperatura en ºC: "))
+match menu:
+    case 1:
+        grados = grados + FARENHEIT
+        print("La conversión es:", grados, "grados Farenheit")
+    case 2:
+        grados = grados + KELVIN
+        print("La conversión es:", grados, "grados Kelvin")
+"""
+"""
+    ## EJERCICIO SUMA
+num = int(input("Introduce un número: "))
+resultado, suma = 0, 0
+while num < 0:
+    num = int(input("Introduce un número: "))
+while num >= resultado + suma:
+    resultado = suma + resultado
+    print(suma, "->", resultado)
+    suma += 1
+"""
+"""
+## EJERCICIO MOSTRAR PALABRAS AL REVÉS
+print((input("Introduce tu frase: ")[::-1]))
+"""
+## EJERCICIO PARA AÑADIR VALORES A UNA LISTA
+print("Si el usuario introduce un 0 la lista se mostrará")
+lista = []
+lista.append(str(input("Introduce valores que quieras añadir a la lista: ")))
+print(lista)
+if __name__ == '__main__':
+    main()
