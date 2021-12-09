d512794b6dc3443ced6302fd9862eb50dce88090
joelrc26 <jrodriguez2@institutmvm.cat>
2021-12-09 16:49:21 +0100

    Ejercicio 3

--- A11.py
+++ A11.py
@@ -1,20 +1,28 @@
 def main():
     """
     ## EJERCICIO 1
-    validnumbers = []
+    numerosPares = []
     num = 30
     for x in range(0, num + 1):
         if x % 2 == 0:
-            validnumbers.append(x)
-    print(validnumbers)
+            numerosPares.append(x)
+    print(numerosPares)
+    """
     """
     ## EJERCICIO 2
-    validnumbers = []
+    numeros8 = []
     num = 1000
     for x in range(0, num + 1):
         if x % 8 == 0:
-            validnumbers.append(x)
-    print(validnumbers)
-
+            numeros8.append(x)
+    print(numeros8)
+    """
+    ## EJERCICIO 3
+    numeros6 = []
+    num = 1000
+    for x in range(0, num + 1):
+        if x == 6:
+            numeros6.append(x)
+    print(numeros6)
 if __name__ == '__main__':
         main()
