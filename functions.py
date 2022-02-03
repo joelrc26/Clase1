c74fba3b8c93d0dc74764af50a3445de38168bbf
joelrc26 <jrodriguez2@institutmvm.cat>
2022-02-03 16:39:35 +0100

    ej 1 con funciones

--- functions.py
+++ functions.py
@@ -1,4 +1,21 @@
 
+def validate():
+    count = 0
+    num = int(input("Introdueix un nombre que estigui entre el 10 i el 5000: "))
+    while (num < 10 or num > 5000) and count < 2:
+        num = int(input("Introdueix un nombre que estigui entre el 10 i el 5000: "))
+        count = count + 1
+    if count == 2:
+        return -1
+    else:
+        return num
+
+def main():
+    num1 = validate()
+    while num1 == -1:
+        print("Te has equivocado 3 veces")
+        num1 = validate()
+    print("Has introducido el ", num1)
 
 if __name__ == '__main__':
     main()
